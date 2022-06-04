d48ae5d7bdccb2b7760897208ece6b4d0cc92fd3
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-06-04 19:57:47 +0530

    yes me

--- index.js
+++ index.js
@@ -106,8 +106,22 @@ app.delete('/deleteOrder/:id', (req, res) => {
     });
 });
 //11 D
-app.post('/addtocart', (req, res) => {
+app.post('/addcart', (req, res) => {
     db.collection('cart').insertMany(req.body, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+// db.coll.update({"_id": 1}, {$push :{"array": 1}})
+// db.coll.update({"_id": 1}, {$push: {scores: {$each: [90, 92, 85]}}})
+//12 D
+app.put('/addtocart/:cartId/:productId/:quantity', (req, res) => {
+    let oid = Number(req.params.cartId);
+    db.collection('cart').updateOne({ cartId: oid }, {
+        $push: {
+            "productIds": [req.params.productId, req.params.quantity]
+        }
+    }, (err, result) => {
         if (err) throw err;
         res.send(result);
     });
