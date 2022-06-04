6654b87a8eb1fb62dff1b141c5fb21c046881df7
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-06-04 08:56:44 +0530

    yes me

---
+++ index.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let cors = require('cors');
+let dotenv = require('dotenv');
+dotenv.config();
+let port = process.env.PORT || 9870;
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+//let mongoUrl = process.env.MongoUrl;
+let mongoUrl = process.env.MongoLiveUrl;
+let db;
+
+// middleware(supporting lib)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+// app.use(express.json());
+
+
+//Connection with db
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) console.log(`Error While Connecting`);
+    db = client.db('restaurantdata');
+    app.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`Express Server listening on port ${port}`)
+    });
+});
